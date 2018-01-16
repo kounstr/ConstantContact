2374b56222e6058b051988ef997ea70dd48443ca
Tracy Kouns <35497714+kounstr@users.noreply.github.com>
2018-01-16 10:45:08 -0600

    Update _merge_csvFiles.ps1
    test server name path

--- _merge_csvFiles.ps1
+++ _merge_csvFiles.ps1
@@ -24,9 +24,10 @@ Write-Output "$OutputFile successfully created"
 
 
 # USAGE:  Merge-CSVFiles -CSVFiles C:\temp\file1.csv,C:\temp\file2.csv -OutputFile c:\temp\output.csv
-# TPS USAGE: Merge-CSVFiles -CSVFiles \\esc-msr-v-ut01\d$\Scripts\ConstantContact\reports\emailed\indianed.csv,\\esc-msr-v-ut01\d$\Scripts\ConstantContact\reports\emailed\charters.csv -OutputFile \\esc-msr-v-ut01\d$\Scripts\ConstantContact\reports\emailed\output.csv
+# TPS USAGE: Merge-CSVFiles -CSVFiles \\esc-msr-v-ut01\d$\Scripts\ConstantContact\reports\indianed.csv,\\esc-msr-v-ut01\d$\Scripts\ConstantContact\reports\charters.csv -OutputFile \\esc-msr-v-ut01\d$\Scripts\ConstantContact\reports\upload\output.csv
+Merge-CSVFiles -CSVFiles \\esc-msr-v-ut01\d$\Scripts\ConstantContact\reports\indianed.csv,\\esc-msr-v-ut01\d$\Scripts\ConstantContact\reports\charters.csv -OutputFile \\esc-msr-v-ut01\d$\Scripts\ConstantContact\reports\upload\output.csv
 
 
-Merge-CSVFiles -CSVFiles reports\academicsupport.csv,reports\indianed.csv,reports\prinall.csv,reports\prinsecall.csv,reports\asstprinall.csv,reports\charters.csv -OutputFile reports\upload\SLmemo.csv
-Merge-CSVFiles -CSVFiles reports\allteach.csv,reports\academicsupport.csv,reports\indianed.csv,reports\prinall.csv,reports\prinsecall.csv,reports\asstprinall.csv,reports\charters.csv -OutputFile reports\upload\TeacherConnect.csv
-Merge-CSVFiles -CSVFiles reports\adminoff.csv,reports\indianed.csv,reports\wilson.csv -OutputFile reports\upload\DistrictDish.csv
+# Merge-CSVFiles -CSVFiles reports\academicsupport.csv,reports\indianed.csv,reports\prinall.csv,reports\prinsecall.csv,reports\asstprinall.csv,reports\charters.csv -OutputFile reports\upload\SLmemo.csv
+# Merge-CSVFiles -CSVFiles reports\allteach.csv,reports\academicsupport.csv,reports\indianed.csv,reports\prinall.csv,reports\prinsecall.csv,reports\asstprinall.csv,reports\charters.csv -OutputFile reports\upload\TeacherConnect.csv
+# Merge-CSVFiles -CSVFiles reports\adminoff.csv,reports\indianed.csv,reports\wilson.csv -OutputFile reports\upload\DistrictDish.csv
